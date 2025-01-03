75d3c4f15caf88350869c1ab384220c817a364c6
ti3c2 <ti3c2@yandex.com>
2025-01-03 12:33:59 +0300

    fix: removed backslashes at the end of lines which were breaking the execution

--- setup-scripts/setup-comfyui/install-models.sh
+++ setup-scripts/setup-comfyui/install-models.sh
@@ -29,12 +29,12 @@ load_sdxl() {
     model_name="SDXL"
     echo "Loading $model_name model..."
     # -nc to prevent loading of existing file
-    wget -nc -P "$MODELS_PATH/checkpoints/SDXL" https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors \
+    wget -nc -P "$MODELS_PATH/checkpoints/SDXL" https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors
 
     echo "Performing $model_name model-specific steps..."
-    wget -nc -P "$MODELS_PATH/loras/HyperSD/SDXL" https://huggingface.co/ByteDance/Hyper-SD/resolve/main/Hyper-SDXL-4steps-lora.safetensors \
+    wget -nc -P "$MODELS_PATH/loras/HyperSD/SDXL" https://huggingface.co/ByteDance/Hyper-SD/resolve/main/Hyper-SDXL-4steps-lora.safetensors
 }
-model_functions["$model_name"]=load_sdxl
+model_functions["SDXL"]=load_sdxl
 
 # Function to load FLUX model
 load_flux() {
@@ -42,21 +42,21 @@ load_flux() {
     # This is checkpoint made by Comfy. It includes VAE and CLIP
     # The original BlackForest does not include them and requires
     # loading VAE and CLIP separately
-    wget -nc -P "$MODELS_PATH/checkpoints/FLUX1" https://huggingface.co/Comfy-Org/flux1-dev/resolve/main/flux1-dev-fp8.safetensors \
+    wget -nc -P "$MODELS_PATH/checkpoints/FLUX1" https://huggingface.co/Comfy-Org/flux1-dev/resolve/main/flux1-dev-fp8.safetensors
 }
 model_functions["FLUX"]=load_flux
 
 # Function to load JuggernautXL model
 load_juggernautxl() {
     echo "Loading JuggernautXL v9..."
-    wget -nc -P "$MODELS_PATH/checkpoints/JuggernautXL" https://huggingface.co/RunDiffusion/Juggernaut-XL-v9/resolve/main/Juggernaut-XL_v9_RunDiffusionPhoto_v2.safetensors \
+    wget -nc -P "$MODELS_PATH/checkpoints/JuggernautXL" https://huggingface.co/RunDiffusion/Juggernaut-XL-v9/resolve/main/Juggernaut-XL_v9_RunDiffusionPhoto_v2.safetensors
 }
 model_functions["JUGGERNAUTXL"]=load_juggernautxl
 
 # Iterate through the models and load them
 for model in "${models[@]}"; do
     if [[ -n "${model_functions[$model]}" ]]; then
-        ${model_functions[$model]}  # Call the function associated with the model
+        ${model_functions[$model]}
     else
         echo "Unknown model name: $model"
         echo "Please set LOAD_MODELS to valid model names (e.g., 'SDXL,FLUX')."
