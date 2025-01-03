be8a64f6d6e896477baccde64291f8254a29d164
ti3c2 <ti3c2@yandex.com>
2025-01-03 12:42:42 +0300

    fix: clone setup repo into /INIT and create symlink
    This fixes issues with github without having to override scripts

--- init.sh
+++ init.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-git clone https://github.com/ti3c2/docker-setups.git /
+# Clone the repository
+git clone https://github.com/ti3c2/docker-setups.git /INIT
 
+# Create a symlink to setup-scripts in the root directory
+ln -s /INIT/setup-scripts /setup-scripts
+
+# Make scripts executable
 chmod -R +x /setup-scripts/
 
-cd setup-scripts
+# Execute
+cd /setup-scripts
 bash setup.sh
