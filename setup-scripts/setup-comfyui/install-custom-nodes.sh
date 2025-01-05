386450e8ed51797715c764b17531d0ce19aef504
ti3c2 <ti3c2@yandex.com>
2025-01-05 11:37:25 +0300

    ci: add plugins

--- setup-scripts/setup-comfyui/install-custom-nodes.sh
+++ setup-scripts/setup-comfyui/install-custom-nodes.sh
@@ -12,6 +12,28 @@ declare -a custom_nodes=(
     "https://github.com/WASasquatch/was-node-suite-comfyui"
     # Text input nodes and much more to research...
     "https://github.com/TinyTerra/ComfyUI_tinyterraNodes"
+    #
+    "https://github.com/ssitu/ComfyUI_UltimateSDUpscale"
+    # Face swap models. The package installs too many models, so maybe it is fine to stop it
+    "https://github.com/Gourieff/comfyui-reactor-node"
+    # Image side-by-side comparison, workspace bookmarks, etc
+    "https://github.com/rgthree/rgthree-comfy"
+    # Show text
+    "https://github.com/pythongosssss/ComfyUI-Custom-Scripts"
+    # Inputs for Int, Float, String
+    "https://github.com/M1kep/ComfyLiterals"
+    # SDXL resolutions and math operations
+    "https://github.com/evanspearman/ComfyMath"
+    # Image size
+    "https://github.com/cubiq/ComfyUI_essentials"
+    # Switches
+    "https://github.com/lquesada/ComfyUI-Interactive"
+    # CLIP Segmentation
+    "https://github.com/time-river/ComfyUI-CLIPSeg"
+    # Unsampler
+    "https://github.com/BlenderNeko/ComfyUI_Noise"
+    # Tools and widgets
+    "https://github.com/pythongosssss/ComfyUI-Custom-Scripts"
 )
 
 # Navigate to custom_nodes folder
