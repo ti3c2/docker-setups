5a8b06338b4d163c3184fd90ac5b7b63bfb2ab1c
ti3c2 <ti3c2@yandex.com>
2025-01-09 14:07:16 +0300

    fix: proper calls of scripts

--- setup-scripts/setup-comfyui/setup-comfyui.sh
+++ setup-scripts/setup-comfyui/setup-comfyui.sh
@@ -5,7 +5,7 @@ pip install --no-cache-dir --upgrade pip setuptools wheel
 pip install --no-cache-dir datasets huggingface-hub "protobuf<4" "click<8.1"
 
 # Set the working directory for ComfyUI
-export COMFY_DIR="{$COMFY_DIR:-$HOME/comfyui}"
+export COMFY_DIR="{$COMFY_DIR:-$HOME/ComfyUI}"
 mkdir -p "$COMFY_DIR"
 cd "$COMFY_DIR" || exit
 
@@ -16,6 +16,6 @@ git clone https://github.com/comfyanonymous/ComfyUI .
 pip install xformers!=0.0.18 --no-cache-dir -r requirements.txt --extra-index-url https://download.pytorch.org/whl/cu121
 
 # Execute installation scripts for components in parallel
-bash "$SETUP_SCRIPTS_DIR/setup-comfyui/install-custom-nodes.sh" &
-bash "$SETUP_SCRIPTS_DIR/setup-comfyui/install-models.sh" "{$LOAD_MODELS}" &
+bash $SETUP_SCRIPTS_DIR/setup-comfyui/install-custom-nodes.sh &
+bash $SETUP_SCRIPTS_DIR/setup-comfyui/install-models.sh "{$LOAD_MODELS}" &
 wait
