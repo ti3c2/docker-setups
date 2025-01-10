409689caf44d3f2a4b28c9adef632473bef91389
ti3c2 <ti3c2@yandex.com>
2025-01-10 08:19:11 +0300

    feat: add execution flow setup to init.sh

--- init.sh
+++ init.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+# Setup execution flow variables
+export PYTHON_VERSION="3.10.12"
+export SETUP_HOME=0
+export SETUP_PYTHON=1
+export SETUP_COMFYUI=1
+export LOAD_MODELS="SDXL FLUX JUGGERNAUTXL"
+
 # Setup path for setup scripts
 export INIT_DIR="$HOME/INIT"
-export SETUP_SCRIPTS_DIR="$SETUP_SCRIPTS_DIR/setup-scripts"
+export SETUP_SCRIPTS_DIR="$INIT_DIR/setup-scripts"
 
 # Clone the repository
-git clone https://github.com/ti3c2/docker-setups.git "$INIT_DIR"
+[ -d "$SETUP_SCRIPTS_DIR" ] || git clone https://github.com/ti3c2/docker-setups.git "$INIT_DIR"
 
 # Make scripts executable
 chmod -R +x "$SETUP_SCRIPTS_DIR"
