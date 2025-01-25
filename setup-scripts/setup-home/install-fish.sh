a8bdb742dc552f0a2335bdba044419c97b3bae25
ti3c2 <ti3c2@yandex.com>
2025-01-25 16:16:22 +0300

    feat: update fish install

--- setup-scripts/setup-home/install-fish.sh
+++ setup-scripts/setup-home/install-fish.sh
@@ -2,10 +2,9 @@
 
 # Add the Fish shell PPA and install Fish
 apt-add-repository ppa:fish-shell/release-3 -y
-apt-get update
-apt-get install -y --no-install-recommends \
+apt update && apt install -y --no-install-recommends \
     software-properties-common gnupg \
     fish
 
 # Set Fish to be default shell
-chsh -s $(which fish)
+chsh -s "$(which fish)"
