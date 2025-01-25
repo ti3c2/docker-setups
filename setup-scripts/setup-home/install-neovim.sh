ee2147697f9e3ba57b856089ab8f718ae833f2db
ti3c2 <ti3c2@yandex.com>
2025-01-25 16:16:05 +0300

    feat: install neovim without build

--- setup-scripts/setup-home/install-neovim.sh
+++ setup-scripts/setup-home/install-neovim.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
+INSTALL_NEOVIM="${INSTALL_NEOVIM:-1}"
+if (( "$INSTALL_NEOVIM" == "0" )); then
+    exit 1
+fi
+
 # Install Neovim and its dependencies
 apt-get update
 apt-get install -y --no-install-recommends \
-    ninja-build gettext cmake unzip curl build-essential git ca-certificates
+    ninja-build gettext cmake unzip curl build-essential git ca-certificates \
+    fzf ripgrep
 
-# Clone Neovim repository and build it
-git clone https://github.com/neovim/neovim /tmp/neovim
-cd /tmp/neovim
-make CMAKE_BUILD_TYPE=RelWithDebInfo
-make install
-rm -rf /tmp/neovim
+VERSION="v0.10.2"
+PACKAGE_NAME="nvim-linux64"
+TAR_NAME="$PACKAGE_NAME.tar.gz"
+wget -nc -P /tmp https://github.com/neovim/neovim/releases/download/$VERSION/$TAR_NAME
+rm -rf /opt/nvim*
+tar -C /opt -xzvf /tmp/$TAR_NAME
+ln -s /opt/$PACKAGE_NAME/bin/nvim /bin/nvim
+rm -rf /tmp/$TAR_NAME
+
 
 # Install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
