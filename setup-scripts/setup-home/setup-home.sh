937aa06598ef3e24ff44e7d9053f085a383704c9
ti3c2 <ti3c2@yandex.com>
2025-01-25 16:17:00 +0300

    feat: improve scripting in setup-home

--- setup-scripts/setup-home/setup-home.sh
+++ setup-scripts/setup-home/setup-home.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
 # Install essentials
-apt-get update
-apt-get install -y --no-install-recommends \
+apt update && apt install -y --no-install-recommends \
     # Install git. Resolve issue with ca-certificates
     git git-lfs ca-certificates \
     # Install Neovim Build essentials
     ninja-build gettext cmake unzip curl build-essential \
     # Install thing responsible for apt-add-repository
     software-properties-common \
+    # Plugins essentials
+    fzf ripgrep \
+    # Other useful things
+    ncdu \
     # Don't remember what this does
     gnupg
 
 # Download dotfiles and symlink them
-git clone https://github.com/poqushoi/my-dotfiles "$HOME/dotfiles"
-git checkout remote-setup
-ln -s "$HOME/dotfiles/.config" "$HOME/.config"
+if [ -n "${DOTFILES_REPO}" ]; then
+    git clone $DOTFILES_REPO "$HOME/dotfiles"
+    git -C "$HOME/dotfiles" checkout remote-setup
+    ln -s "$HOME/dotfiles/.config" "$HOME/.config"
+    # cd $HOME/dotfiles || exit
+    # stow .
+fi
 
 # Execute scripts
-bash "$SETUP_SCRIPTS_DIR/setup-home/install-neovim.sh"
-bash "$SETUP_SCRIPTS_DIR/setup-home/install-fish.sh"
-
+bash $SETUP_SCRIPTS_DIR/setup-home/install-neovim.sh
+bash $SETUP_SCRIPTS_DIR/setup-home/install-fish.sh
 
 # Clean apt cache
 apt-get clean && rm -rf /var/lib/apt/lists/*
